9f8a3f86c2d839d35e538ff4867d7ed3e540bf1d
Lachelle Pavlovic <lachelle.anne@gmail.com>
2021-09-24 23:32:50 +1000

    dark mode added

---
+++ script.js
@@ -0,0 +1,39 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+
+//Dark Mode
+function darkMode(){
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');   
+}
+
+//Light Mode
+function lightMode(){
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+}
+
+//Switch Theme
+function switchTheme(event){
+    if (event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    }
+    else{
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
+
+//Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+//Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
